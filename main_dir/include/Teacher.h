749f6fd6f96deccd873d99c74ebb5f68c2b1d477
Aniket2027 <creatfiles5@gmail.com>
2024-03-24 14:45:17 +0530

    Entry function in Teacher.cpp

--- main_dir/include/Teacher.h
+++ main_dir/include/Teacher.h
@@ -2,15 +2,18 @@
 #define TEACH_H
 
 #include <iostream>
+using namespace std;
 
 class Teacher
 {
 
-    // static int nos;     // nos -- number of student
+    static int sno;     // nos -- number of student
 
 public:
     void teach_home();
     void student_entry();
+    void gen_cred(int, string);
+    int sno_upd();
 };
 
 #endif
